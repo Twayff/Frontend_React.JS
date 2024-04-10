2c54f4d23496f09640b1a2531239c029eb086938
Twayff <thiagolacerda.gtl@hotmail.com>
2024-04-10 17:47:53 -0300

    props_hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import Home from './paginas/home/Home';
 import './App.css';
-
+import React from 'react';
 
 function App() {
+ 
   return (
-   <Home  title="Componente Home"
-          description="Este é um componente Home que recebe props."/>
+    <>
+      <Home/>
+    </>
   );
 }
 
